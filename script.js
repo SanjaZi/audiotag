e101e947d01f8d5a4403ecc581b63d434091fb31
Sanja Zivkovic <80321654+SanjaZi@users.noreply.github.com>
2023-02-27 16:32:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const audioclass = document.querySelectorAll(".audio");
+const audiotest = document.querySelectorAll(".audiotest");
+
+
+audioclass.forEach(element => {
+  element.addEventListener('click', function() {
+        audioclass.forEach(function (el, index) {
+        el.classList.remove('active');
+        audiotest[index].pause();    
+        });
+        element.classList.add('active');
+
+// Get all the elements with the "active" class
+    const activeElements = document.querySelectorAll('.active');
+
+// Loop through each active element and get its associated audio element
+    activeElements.forEach(activeElement => {
+    const audio = activeElement.querySelector('audio');
+    audio.play(); 
+    });
+  });
+});
+
+
