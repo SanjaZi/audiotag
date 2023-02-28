7e3fa49b1c78c90cf5b16a8edca6c5124e32e4c4
Sanja Zivkovic <80321654+SanjaZi@users.noreply.github.com>
2023-02-28 10:59:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const audioclass = document.querySelectorAll(".audio");
 const audiotest = document.querySelectorAll(".audiotest");
 
-
 audioclass.forEach(element => {
   element.addEventListener('click', function() {
         audioclass.forEach(function (el, index) {
         el.classList.remove('active');
-        audiotest[index].pause();    
+        audiotest[index].pause();
+        audiotest[index].currentTime = 0;  
         });
         element.classList.add('active');
+        
 
 // Get all the elements with the "active" class
     const activeElements = document.querySelectorAll('.active');
@@ -16,9 +17,9 @@ audioclass.forEach(element => {
 // Loop through each active element and get its associated audio element
     activeElements.forEach(activeElement => {
     const audio = activeElement.querySelector('audio');
-    audio.play(); 
+    audio.play();
+
     });
+    
   });
 });
-
-
